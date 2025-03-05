664a121dafa76299c315b6366daaaf52ad981d19
unknown <samehrizk1993@gmail.com>
2025-03-05 15:27:57 +0200

    modifing app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import ProductsList from "./components/ProductsList";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductsList />} />
+      </Routes>
     </>
   );
 }
-
 export default App;
